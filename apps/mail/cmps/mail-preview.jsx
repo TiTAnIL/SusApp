c54cc8a39b17ca5e1aca00c63b1c61b7cbfadd48
Denis <denismagin@gmail.com>
2022-08-24 22:48:25 +0300

    minor html changes

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,14 +1,17 @@
 export function MailPreview({ mail, onRemoveMail }) {
 
 
-    return <article key={`container${mail.id}`} className="mail-container">
-        <button key={`check-btn${mail.id}`} className="Check-btn">Check</button>
-        <button key={`star-btn${mail.id}`} className="star-btn">Star</button>
-        <div key={`title${mail.id}`} className="mail-title">{mail.title}</div>
-        <div key={`content${mail.id}`} className="mail-content">{mail.about}</div>
-        <button key={`archive-btn${mail.id}`} className="archive-btn">archive</button>
-        <button key={`del-btn${mail.id}`} className="delete-btn" onClick={()=>onRemoveMail(mail.id)}>X</button>
-        <button key={`mark-btn${mail.id}`} className="mark-btn">mark</button>
-    </article>
-
+    return <tr key={`container${mail.id}`} className="mail-body">
+        <td key={`check-btn${mail.id}`} className="Check-btn" itemType="checkbox" >Check</td>
+        <td key={`star-btn${mail.id}`} className="star-btn">Star</td>
+        <td>
+            <span key={`title${mail.id}`} className="mail-title">{mail.title}</span>
+        </td>
+        <td>
+            <span key={`content${mail.id}`} className="mail-content">{mail.about}</span>
+        </td>
+        <td key={`archive-btn${mail.id}`} className="archive-btn">archive</td>
+        <td key={`del-btn${mail.id}`} className="delete-btn" onClick={()=>onRemoveMail(mail.id)}>X</td>
+        <td key={`mark-btn${mail.id}`} className="mark-btn">mark</td>
+    </tr>
 }
