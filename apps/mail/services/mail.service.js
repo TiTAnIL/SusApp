c528b534e81eca191a236e0b0cb7dbae3f7e8dcf
Denis <denismagin@gmail.com>
2022-08-24 17:57:06 +0300

    added basic mail skeleton

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,13 +1,17 @@
-import { storageService } from "../../../services/storage.service"
+import { storageService } from "../../../services/storage.service.js"
+
+const KEY = 'mailsDB'
 
 export const mailService = {
     query,
 }
 
-KEY = 'mailsDB'
+
+function getMails() {
+    return gMails
+}
 
 function query() {
-    debugger
     let mails = _loadFromStorage()
     if (!mails) {
         mails = getMails()
@@ -17,10 +21,6 @@ function query() {
 }
 
 
-function getMails() {
-    console.log('getting mails from gMails')
-    return gMails
-}
 
 
 function _saveToStorage(val) {
@@ -34,75 +34,83 @@ function _loadFromStorage() {
 
 const gMails = [
     {
-        "_id": "630611d75d246bcdd90ba61c",
+        "_id": "63063a9b0fce165fdfba7e07",
         "index": 0,
-        "guid": "b6184126-0af3-4688-92e2-2df452e3c22e",
-        "isActive": false,
-        "name": "Candy Cannon",
-        "email": "candycannon@quantasis.com",
-        "about": "Ipsum dolor aliqua occaecat enim id nulla excepteur dolor. Magna tempor minim exercitation veniam in quis laborum est. Cupidatat minim eu do ullamco qui duis nulla ad. Non incididunt dolor Lorem minim enim aliqua ipsum magna et do consequat. Nulla exercitation non esse labore est. Aliqua amet consectetur duis consectetur id ea exercitation sunt aliquip occaecat aute dolor ipsum reprehenderit. Cupidatat adipisicing qui consectetur incididunt officia labore nisi sunt anim aliquip Lorem est.\r\n"
+        "guid": "84b8cf98-edec-4adb-8618-10edf617a3dd",
+        "isActive": true,
+        "name": "Lorie Stephenson",
+        "email": "loriestephenson@edecine.com",
+        "about": "Sit elit dolore dolore commodo laboris qui irure nisi consequat. Eu duis duis id labore qui qui. Minim incididunt dolor deserunt dolor. Consequat ad esse aliquip consectetur excepteur exercitation culpa est quis in ut non culpa est. Nostrud do adipisicing adipisicing pariatur et et magna.\r\n",
+        "title": "do non sit magna Lorem"
     },
     {
-        "_id": "630611d71bb26a689610f8cf",
+        "_id": "63063a9bc94cebb08bd90fe5",
         "index": 1,
-        "guid": "71966be5-d1ca-4634-8091-66ca7efd8fd6",
+        "guid": "99cffa0a-bd77-45ab-a60e-efbf8ebb7941",
         "isActive": true,
-        "name": "Jackson Oneil",
-        "email": "jacksononeil@quantasis.com",
-        "about": "Magna labore qui eiusmod irure excepteur sit sunt et sit velit minim deserunt ex sint. Velit nostrud est amet ut velit qui ex aliqua nulla ullamco. Consequat ipsum duis aute et aute sit. Ipsum est tempor esse cupidatat sint reprehenderit ea laboris voluptate anim consequat irure.\r\n"
+        "name": "Velez Rowland",
+        "email": "velezrowland@edecine.com",
+        "about": "Labore enim veniam officia minim ullamco. Dolor cupidatat dolor ut id voluptate. Voluptate non enim quis dolor nostrud reprehenderit dolore. Nulla qui fugiat elit eu laborum voluptate do voluptate voluptate cupidatat veniam cillum cillum. Reprehenderit sit enim proident dolor sit.\r\n",
+        "title": "irure officia velit in pariatur"
     },
     {
-        "_id": "630611d79e2c7a40c0f34701",
+        "_id": "63063a9b3875598c00e33fdb",
         "index": 2,
-        "guid": "feab1208-d34b-4ddb-9ed8-d39b8b0fe82b",
+        "guid": "825bc163-4d7d-43a4-a74e-34b217d0f18c",
         "isActive": true,
-        "name": "Peggy Wolfe",
-        "email": "peggywolfe@quantasis.com",
-        "about": "Proident irure nostrud magna eiusmod mollit esse aliquip qui Lorem. Et anim est ad ullamco. Ea sit dolor anim consectetur.\r\n"
+        "name": "Jayne Nieves",
+        "email": "jaynenieves@edecine.com",
+        "about": "Pariatur dolor reprehenderit qui elit aliqua exercitation sit sunt. Proident dolor sit est elit labore deserunt voluptate tempor laboris qui aute. Occaecat sunt ullamco anim adipisicing tempor ea officia reprehenderit incididunt velit consequat amet fugiat commodo. Eu enim magna adipisicing minim ut aliquip labore id tempor commodo eiusmod sit.\r\n",
+        "title": "laboris laborum dolore minim velit"
     },
     {
-        "_id": "630611d77ce46919ed80b4f6",
+        "_id": "63063a9b6776466315940a0c",
         "index": 3,
-        "guid": "0e676555-863a-4485-a797-a1c7f7df5624",
+        "guid": "0c475275-fd20-4609-b35e-46df821494b1",
         "isActive": true,
-        "name": "Potts Hudson",
-        "email": "pottshudson@quantasis.com",
-        "about": "Irure mollit qui ad aute esse. Eiusmod pariatur consequat commodo id id consequat ullamco proident consectetur pariatur reprehenderit Lorem pariatur ullamco. Excepteur consectetur mollit incididunt consectetur eiusmod esse. Veniam commodo labore cupidatat officia mollit reprehenderit culpa laborum sint ut eu velit ut. Aliqua cupidatat aliquip ea Lorem voluptate sunt elit exercitation consequat aliqua velit ex sint dolor.\r\n"
+        "name": "Tasha Morgan",
+        "email": "tashamorgan@edecine.com",
+        "about": "Incididunt occaecat eiusmod exercitation amet exercitation fugiat dolor occaecat officia ipsum minim duis. Esse consequat esse tempor ex ut incididunt irure veniam qui. Eu cillum qui aute adipisicing eu Lorem esse est. Eiusmod esse proident in veniam tempor occaecat tempor ullamco esse ea dolor commodo exercitation. Commodo sunt ex proident magna excepteur dolore magna.\r\n",
+        "title": "nisi dolore eu officia magna"
     },
     {
-        "_id": "630611d7a77211a412682f28",
+        "_id": "63063a9b4fa08de4d4a15265",
         "index": 4,
-        "guid": "06d088be-a9d1-4df6-802a-1446f7b82cca",
+        "guid": "fed37cb2-aec6-49d8-bb37-ba3d33406069",
         "isActive": true,
-        "name": "Wilkerson Byers",
-        "email": "wilkersonbyers@quantasis.com",
-        "about": "Mollit minim ut laborum enim in deserunt ullamco sint magna. Nostrud consequat voluptate exercitation enim ipsum aute ullamco. Tempor velit est et est ut magna sint sint officia esse.\r\n"
+        "name": "Leanna Mercado",
+        "email": "leannamercado@edecine.com",
+        "about": "Ullamco elit aliquip ipsum fugiat magna consequat reprehenderit dolore deserunt laborum proident veniam. Non officia adipisicing reprehenderit amet nulla tempor mollit et ea. Sunt adipisicing laborum mollit fugiat minim nulla quis ullamco. Non mollit consectetur laboris laborum. Cillum pariatur sint dolor commodo id enim voluptate ea.\r\n",
+        "title": "aute fugiat reprehenderit qui esse"
     },
     {
-        "_id": "630611d78aa4d040a86af531",
+        "_id": "63063a9bc8bab74cb5efeed2",
         "index": 5,
-        "guid": "41081fc1-498c-4487-96b2-d83fb7033a39",
+        "guid": "43b3acaf-eccc-4711-8b00-fd6341dfc48b",
         "isActive": true,
-        "name": "Walton Bernard",
-        "email": "waltonbernard@quantasis.com",
-        "about": "Sunt ullamco sit qui aliqua velit. Dolore aliquip sit ad ullamco adipisicing et. Enim exercitation exercitation ea esse in incididunt pariatur. Elit non ipsum aliqua sint sint duis id amet aliqua Lorem. Magna velit nostrud labore ipsum do duis consequat proident velit culpa ipsum laborum. Mollit occaecat est pariatur incididunt aliqua voluptate esse exercitation. Laboris sit nisi culpa anim velit irure minim quis nisi ad.\r\n"
+        "name": "Ina Lowery",
+        "email": "inalowery@edecine.com",
+        "about": "Veniam est voluptate velit reprehenderit id aute enim quis. Laborum est commodo amet aliqua adipisicing sint deserunt veniam quis nulla voluptate sunt. Laborum et nisi sunt proident ipsum reprehenderit officia sit irure. Esse irure occaecat ipsum nulla consectetur.\r\n",
+        "title": "ipsum deserunt laborum minim ut"
     },
     {
-        "_id": "630611d75a18fded39d64662",
+        "_id": "63063a9bfeb94a54d3178011",
         "index": 6,
-        "guid": "96d5a058-493c-46b9-9f24-b3d96559b06c",
-        "isActive": false,
-        "name": "Anderson Brennan",
-        "email": "andersonbrennan@quantasis.com",
-        "about": "Eu tempor ex nisi ad qui deserunt. Esse cillum qui amet adipisicing ad in officia occaecat anim labore sunt. Sint irure non laboris qui duis commodo commodo elit et dolore. Do nostrud enim adipisicing velit enim consectetur duis Lorem aliquip qui reprehenderit.\r\n"
+        "guid": "143ec27a-819c-4897-8924-53f476cb7df0",
+        "isActive": true,
+        "name": "Patricia Reese",
+        "email": "patriciareese@edecine.com",
+        "about": "Ullamco qui ipsum do nisi consectetur officia deserunt. Occaecat eiusmod irure eu amet elit mollit pariatur non cillum eiusmod. Nulla officia aute mollit ipsum. Labore tempor ullamco eiusmod duis. Est cillum eiusmod mollit commodo sint minim.\r\n",
+        "title": "occaecat nostrud dolore eu ut"
     },
     {
-        "_id": "630611d7556ef8fcdc0bc16b",
+        "_id": "63063a9b120fed65a7f61e5e",
         "index": 7,
-        "guid": "6981fb6b-8494-41a9-a909-a7c0b7034def",
+        "guid": "f261635c-e011-400d-8c8b-13f837151356",
         "isActive": false,
-        "name": "Renee Case",
-        "email": "reneecase@quantasis.com",
-        "about": "Ea cupidatat nulla magna reprehenderit sint exercitation incididunt ullamco amet voluptate. Laborum sunt consequat minim Lorem et ex dolor excepteur. Ut occaecat eiusmod laboris magna. Ad occaecat reprehenderit et sunt qui culpa elit in cillum veniam officia cillum sunt culpa. Ad sint id eu velit irure aliquip.\r\n"
+        "name": "West Stafford",
+        "email": "weststafford@edecine.com",
+        "about": "Exercitation ipsum esse in pariatur proident deserunt pariatur. Qui anim ut deserunt sunt sunt eu sit id anim ad aute nisi. Nulla non minim aliqua occaecat deserunt minim ut occaecat proident aute consectetur ea sit. Ad ex elit dolor minim velit.\r\n",
+        "title": "ipsum anim labore eu aute"
     }
 ]
