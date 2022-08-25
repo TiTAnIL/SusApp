f0813ad3fc6971e9e2490c862db972eaf51ded46
EldadSarussi <ems1487@gmail.com>
2022-08-25 14:03:25 +0300

    added pictures

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -4,6 +4,6 @@ import { NotePreview } from "./note-preview.jsx"
 export function NoteList({ notes }) {
 
     return <section className="notes-app">
-        {notes.map(note => <NotePreview key={note.id} note={note} />)}
+        {notes.map(note => <NotePreview className="note" key={note.id} note={note} />)}
     </section>
 }
